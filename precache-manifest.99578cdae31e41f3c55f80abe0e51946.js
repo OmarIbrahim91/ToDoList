c441f5f9bb7e1ea88a27fb91f5e6b769a5877477
root <omarali43620@yahoo.com>
2020-02-03 12:59:55 +0100

    Updates

---
+++ precache-manifest.99578cdae31e41f3c55f80abe0e51946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac071821d1ba5264fba6fd0a912226bb",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "2f5d8a1f05a127c581a9",
+    "url": "/ToDoList/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2798ad885847f98649eb",
+    "url": "/ToDoList/static/js/2.ba3b9b39.chunk.js"
+  },
+  {
+    "revision": "2f5d8a1f05a127c581a9",
+    "url": "/ToDoList/static/js/main.3cb4dbf4.chunk.js"
+  },
+  {
+    "revision": "b995e46907af79b9e2e0",
+    "url": "/ToDoList/static/js/runtime-main.56a06689.js"
+  }
+]);
